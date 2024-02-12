55cf9fae908c2d90052d64f45f839d3d85084020
sergio <sergioscardigno82@gmail.com>
2024-02-12 09:50:50 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module activador
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"runtime"
+	"strings"
+)
+
+func runCommand(command string) (string, error) {
+	cmd := exec.Command("powershell", "-Command", command)
+	output, err := cmd.CombinedOutput()
+	return string(output), err
+}
+
+func main() {
+	// Verificar si se está ejecutando en Windows
+	if runtime.GOOS != "windows" {
+		fmt.Println("Este programa está diseñado para ejecutarse en Windows.")
+		return
+	}
+
+	// Verificar si se está ejecutando como administrador
+	cmd := "([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)"
+	output, err := runCommand(cmd)
+	if err != nil || strings.TrimSpace(output) != "True" {
+		fmt.Println("Debes ejecutar este programa como administrador.")
+		return
+	}
+
+	// Obtener la edición del sistema operativo
+	cmd = "(Get-CimInstance Win32_OperatingSystem).Caption"
+	output, err = runCommand(cmd)
+	if err != nil {
+		fmt.Println("Error al obtener la edición del sistema operativo.")
+		return
+	}
+	osEdition := strings.TrimSpace(output)
+
+	// Ruta al archivo JSON de licencias
+	jsonFile := "licencias.json"
+
+	// Leer el contenido del archivo JSON
+	fileContent, err := ioutil.ReadFile(jsonFile)
+	if err != nil {
+		fmt.Println("Error al leer el archivo JSON de licencias.")
+		return
+	}
+
+	// Crear un mapa para almacenar las licencias
+	var licenses map[string]string
+	err = json.Unmarshal(fileContent, &licenses)
+	if err != nil {
+		fmt.Println("Error al analizar el contenido del archivo JSON.")
+		return
+	}
+
+	// Buscar la licencia correspondiente
+	licenseKey, found := licenses[osEdition]
+
+	// Mostrar la licencia
+	if found {
+		fmt.Printf("La licencia para la edición %s es: %s\n", osEdition, licenseKey)
+	} else {
+		fmt.Printf("No se encontró una licencia para la edición %s en el archivo JSON.\n", osEdition)
+	}
+}
